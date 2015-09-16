4156616f77bcd8ad4cbdd110f9a61e5345faabfb
Emir <emcho.pwn@gmail.com>
2015-09-16 20:22:14 +0200

    Saving to session

--- client/templates/gridstack/gridstack.js
+++ client/templates/gridstack/gridstack.js
@@ -1,10 +1,33 @@
 Template.gridstack.helpers({
     tile: function () {
-        return GridTiles.find();
+        //return GridTiles.find();
+
+        var stream = {
+            x: 0,
+            y: 1,
+            width: 2,
+            height: 2,
+            url: "ee"
+        };
+
+        Session.set("nesto", stream);
+
+        var streamArray = [];
+        for (var key in Session.keys) {
+            var object = JSON.parse(Session.keys[key]);
+            streamArray.push({
+                x: object.x,
+                y: object.y,
+                width: object.width,
+                height: object.height,
+                url: object.url
+            });
+        }
+
+        return streamArray;
     },
 });
 Template.gridstack.rendered = function () {
-// Instantiate grid
     var gridstackOptions = {};
     $('.grid-stack').gridstack(gridstackOptions);
 
@@ -12,14 +35,24 @@ Template.gridstack.rendered = function () {
     $('.grid-stack').on('change',function (event, items) {
         _.each(items, function (item) {
             var attributes = item.el.data();
-            GridTiles.update({_id:attributes.panelId}, {$set:{x:attributes.gsX,
-                y:attributes.gsY,height:attributes.gsHeight, width:attributes.gsWidth}});
+            //GridTiles.update({_id:attributes.panelId}, {$set:{x:attributes.gsX,
+            //    y:attributes.gsY,height:attributes.gsHeight, width:attributes.gsWidth}});
+            Session.set({_id:attributes.panelId}, {x:attributes.gsX, y:attributes.gsY, height:attributes.gsHeight, width: attributes.gsWidth});
+
+            $('.grid-stack-item-content').each(function(index) {
+                console.log($(this))
+                //console.log($(this).children()[1].width);
+                //console.log($(this).children()[1].height);
+                //console.log($(this)[0].offsetWidth);
+                //console.log($(this)[0].offsetHeight);
+                $(this).children()[0].width = $(this)[0].offsetWidth;
+                $(this).children()[0].height = $(this)[0].offsetHeight - 20;
+            });
         });
 
     });
 
     Template.gridstackItem.rendered = function () {
-        // For each widget that is created, we use add_widget to associate it with the grid
         var grid = $('.grid-stack').data('gridstack');
         grid.add_widget(this.$('.grid-stack-item'));
     };
@@ -30,9 +63,14 @@ Template.gridstackItem.events({
         e.preventDefault();
 
         var stream = {
+            x: 0,
+            y: 1,
+            width: 2,
+            height: 2,
             url: $(e.target).find('[name=url]').val()
         };
 
-        GridTiles.insert(stream);
+        //GridTiles.insert(stream);
+        Session.set("nesto", stream);
     }
 });
