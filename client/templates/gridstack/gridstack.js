b1b949a64520a2e4fb8b8c811faa019d99ecd8b1
Emir <emcho.pwn@gmail.com>
2015-09-16 23:58:44 +0200

    Removed guid

--- client/templates/gridstack/gridstack.js
+++ client/templates/gridstack/gridstack.js
@@ -1,35 +1,6 @@
 Template.gridstack.helpers({
     tile: function () {
         return TempGridTiles.find();
-
-        //var stream = {
-        //    x: 0,
-        //    y: 1,
-        //    width: 2,
-        //    height: 2,
-        //    url: "ee"
-        //};
-        //
-        //Session.set("nesto", stream);
-
-        //var streamArray = [];
-        //for (var key in Session.keys) {
-        //    var object = JSON.parse(Session.keys[key]);
-        //    streamArray.push({
-        //        x: object.x,
-        //        y: object.y,
-        //        width: object.width,
-        //        height: object.height,
-        //        url: object.url
-        //    });
-        //}
-        //Tracker.autorun(function() {
-        //    Session.get("alo");
-        //    console.log(Session);
-        //});
-        //return Session.get("alo");
-
-        //return Template.instance().addStream.get();
     }
 });
 
@@ -37,12 +8,6 @@ Template.gridstack.rendered = function () {
     var gridstackOptions = {};
     $('.grid-stack').gridstack(gridstackOptions);
 
-    //$('.grid-stack-item-content').each(function(index) {
-    //    $(this).children()[0].width = $(this)[0].offsetWidth;
-    //    $(this).children()[0].height = $(this)[0].offsetHeight - 20;
-    //});
-
-    // add onChange event listener
     $('.grid-stack').on('change',function (event, items) {
         _.each(items, function (item) {
             var attributes = item.el.data();
@@ -51,10 +16,6 @@ Template.gridstack.rendered = function () {
 
             $('.grid-stack-item-content').each(function(index) {
                 console.log($(this))
-                //console.log($(this).children()[1].width);
-                //console.log($(this).children()[1].height);
-                //console.log($(this)[0].offsetWidth);
-                //console.log($(this)[0].offsetHeight);
                 $(this).children()[0].width = $(this)[0].offsetWidth;
                 $(this).children()[0].height = $(this)[0].offsetHeight - 30;
             });
--- client/templates/stream/add_stream.js
+++ client/templates/stream/add_stream.js
@@ -6,20 +6,10 @@ Template.addStream.events({
             x: 0,
             y: 0,
             width: 2,
-            height: 2,
+            height: 3,
             url: $(e.target).find('[name=url]').val()
         };
 
         TempGridTiles.insert(stream);
     }
 });
-
-function guid() {
-    function s4() {
-        return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-    }
-    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-        s4() + '-' + s4() + s4() + s4();
-}
